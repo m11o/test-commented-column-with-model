300b6b2b59dd7870b8166e87608bbd421b76218d
Masahito Osako <masahito.osako@hey.com>
2023-12-03 13:09:18 +0900

    change non-null for users#name

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_034600) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_03_040324) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", comment: "first_name + last_name"
+    t.string "name", null: false, comment: "first_name + last_name"
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
